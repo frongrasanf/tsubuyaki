b9f582412fa1b64f33573226887e245180f4248a
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-05-12 17:32:51 +0900

    設定ファイル編集

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,15 @@ module Tsubuyaki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
 
+    config.generators do |g|
+      g.helper               false
+      g.stylesheets          false
+      g.javascripts          false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
