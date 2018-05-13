fce42a36855e51f5ba9d5d68b6c9254dca613b35
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-05-13 12:59:38 +0900

    add create method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,20 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save!
+      redirect_to tweet_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:text).merge(user_id: current_user.id)
+  end
 end
