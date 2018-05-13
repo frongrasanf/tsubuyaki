4bd7fe0adb753ee5a6cff87ef0fe684758c1ed6f
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-05-13 12:55:46 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'tweets#index'
-  get 'tweets/index'
+  resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
