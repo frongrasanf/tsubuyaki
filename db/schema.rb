d9619bd2885dd9b2750478aba219e049bb95c650
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-05-13 12:39:40 +0900

    add text to tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_031555) do
+ActiveRecord::Schema.define(version: 2018_05_13_033729) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "text"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
